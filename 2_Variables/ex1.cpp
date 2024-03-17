8afd8bddee0ef54980a9f5e5a317dd8da6fca32a
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-17 21:03:18 +0530

    Updated

---
+++ 2_Variables/ex1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int a = 4;
+    int b = 21;
+    int sum = a + b;
+    cout << "The sum of " << a << " and " << b << " is " << sum << endl;
+
+    return 0;
+}
