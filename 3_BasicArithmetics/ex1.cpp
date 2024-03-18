f52064634801613ff570b3c4cb9371f01fd089cd
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-18 20:42:59 +0530

    updated

--- 3_BasicArithmetics/ex1.cpp
+++ 3_BasicArithmetics/ex1.cpp
@@ -3,8 +3,24 @@
 using namespace std;
 
 int main() {
+
     int x = 8/3;
     cout << x << endl;  // Output: 2
+
+    int y = 24 % 7;
+    cout << y << endl;  // Output: 3
+
+    float eq1 = 6 / 4;
+    cout << (float)eq1 << endl;
+
+    float eq = 6.0 * 4.0 + 8.0 * 4.0 + 9.0 / 20.0;
+    cout << eq << endl;  // Output: 56.45
+
+    int a = 4 + 3 * 7;
+    cout << a << endl;  // Output: 25
+
+    int b = (4 + 3) * 7;
+    cout << b << endl;  // Output: 49
 
     return 0;
 }
