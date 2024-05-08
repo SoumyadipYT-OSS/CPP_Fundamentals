c20f39283308519c3f311b91eeefeeedabb48e6c
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-05-08 13:44:36 +0530

    updated

---
+++ 15_DefaultArguments/ex1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int volume(int l=1, int w=1, int h=1);
+
+
+int main() {
+    cout << volume() << endl;
+    cout << volume(5) << endl;
+    cout << volume(5, 6) << endl;
+    cout << volume(5, 6, 7) << endl;
+    return 0;
+}
+
+int volume (int l, int w, int h) {
+    return l*w*h;
+}
