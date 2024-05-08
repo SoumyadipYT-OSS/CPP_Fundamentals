0f676a73884a838f1db1f0eb7604972665d971f7
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-05-08 21:43:54 +0530

    updated

---
+++ 19_Arrays/ex1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {66, 75, 100, 43, 99};
+    cout << arr[2] << endl;
+
+    return 0;
+}
---
+++ 19_Arrays/ex3.cpp
@@ -0,0 +1,7 @@
+class MyClass {
+public:
+    MyClass(int x) : value(x) {}
+    int value;
+};
+
+MyClass arr[] = {MyClass(1), MyClass(2), MyClass(3)};
