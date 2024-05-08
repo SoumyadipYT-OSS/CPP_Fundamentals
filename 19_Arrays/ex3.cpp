e6883caddc6aa14c224a4f7e1e7ce77850fd76b7
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-05-08 21:55:44 +0530

    updated

--- 19_Arrays/ex3.cpp
+++ 19_Arrays/ex3.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+
 class MyClass {
 public:
     MyClass(int x) : value(x) {}
@@ -5,3 +8,18 @@ public:
 };
 
 MyClass arr[] = {MyClass(1), MyClass(2), MyClass(3)};
+
+
+// print the MyClass objects in the array
+void displayArray(MyClass arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i].value << ", ";
+    }
+    cout << endl;
+}
+
+
+int main() {
+    displayArray(arr, 3);
+    return 0;
+}
