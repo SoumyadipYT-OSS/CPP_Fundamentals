edf2c1611f062c17f2c28e8064c5b36d3b6b1aec
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-05-08 13:32:26 +0530

    updated

--- Projects/pr3_RandomNumberGenerator.cpp
+++ Projects/pr3_RandomNumberGenerator.cpp
@@ -1,1 +1,21 @@
 // Random Number Generator
+
+#include <iostream>
+#include <cstdlib>  // Required for rand() and srand()
+#include <ctime>    // Required for time()
+using namespace std;
+
+int main() {
+    // Get the system time
+    unsigned seed = time(0);
+    cout << "Seed: " << seed << endl;
+
+    // Seed the random number generator
+    srand(seed);    // srand() is used to seed the random number generator
+
+    // Generate a random number
+    cout << "Here is a random number: " << rand() << endl;
+
+    return 0;
+    
+}
