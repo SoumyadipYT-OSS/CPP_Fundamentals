59fd54edea5b4e16d4b447aedf2768362fe13dec
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-05-09 13:57:36 +0530

    updated

---
+++ 21_Pointers/ex2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 1025;
+    int *p;
+    p = &a;
+
+    cout << "Size of integer: " << sizeof(int) << " bytes" << endl;
+    cout << "Address = " << p << ", value = " << *p << endl;
+
+    char *p0;
+    p0 = (char*)p; // Typecasting
+    cout << "Size of char: " << sizeof(char) << " bytes" << endl;
+    cout << "Address = " << p0 << ", value = " << *p0 << endl;
+
+    return 0;
+}
