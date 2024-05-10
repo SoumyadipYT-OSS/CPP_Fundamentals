0ad61e874a83601364c28a50b3c4d2c2ca9330d1
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-05-10 15:33:05 +0530

    updated files

--- 21_Pointers/ex3_AllTypesPointer.cpp
+++ 21_Pointers/ex3_AllTypesPointer.cpp
@@ -3,54 +3,69 @@
 #include <iostream>
 using namespace std;
 
-class Solution {
-    int IntegerPointer() {
+class Solution
+{
+public:
+    int IntegerPointer()
+    {
         int a = 1025;
         int *p;
         p = &a;
 
         cout << "Size of integer: " << sizeof(int) << " bytes" << endl;
-        cout << "Size of integer pointer 'p': " << sizeof(int*) << " bytes" << endl;
+        cout << "Size of integer pointer 'p': " << sizeof(int *) << " bytes" << endl;
         cout << "Address = " << p << ", value = " << *p << endl;
 
         return 0;
     }
 
-
-    int FloatingPointer() {
+    int FloatingPointer()
+    {
         float b = 113.45f;
         float *q;
         q = &b;
 
         cout << "Size of float: " << sizeof(float) << " bytes" << endl;
-        cout << "Size of float pointer 'q': " << sizeof(float*) << " bytes" << endl;
+        cout << "Size of float pointer 'q': " << sizeof(float *) << " bytes" << endl;
         cout << "Address = " << q << ", value = " << *q << endl;
 
         return 0;
     }
 
-
-    int DoublePointer() {
+    int DoublePointer()
+    {
         double c = 123.456;
         double *r;
         r = &c;
 
         cout << "Size of double: " << sizeof(double) << " bytes" << endl;
-        cout << "Size of double pointer 'r': " << sizeof(double*) << " bytes" << endl;
+        cout << "Size of double pointer 'r': " << sizeof(double *) << " bytes" << endl;
         cout << "Address = " << r << ", value = " << *r << endl;
 
         return 0;
     }
 
-
-    int VoidPointer() {
+    int VoidPointer()
+    {
         void *vp;
         int a = 1025;
         vp = &a;
 
-        cout << "Size of void pointer 'vp': " << sizeof(void*) << " bytes" << endl;
+        cout << "Size of void pointer 'vp': " << sizeof(void *) << " bytes" << endl;
         cout << "Address = " << vp << endl;
 
         return 0;
     }
 };
+
+// main function
+int main()
+{
+    Solution s;
+    s.IntegerPointer();
+    s.FloatingPointer();
+    s.DoublePointer();
+    s.VoidPointer();
+
+    return 0;
+}
