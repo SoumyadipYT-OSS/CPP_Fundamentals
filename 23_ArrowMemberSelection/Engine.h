ad58a00ea61b4fc9d5b5352a23a7fa48c8add14e
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-05-14 01:17:43 +0530

    updated

---
+++ 23_ArrowMemberSelection/Engine.h
@@ -0,0 +1,13 @@
+# ifndef ENGINE_H
+# define ENGINE_H
+
+class Engine
+{
+    public:
+        Engine();
+        void printStatement();
+    protected:
+    private:
+};
+
+#endif
---
+++ 23_ArrowMemberSelection/ex1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Engine.cpp"
+
+using namespace std;
+
+int main() {
+    Engine engObj;
+    engObj.printStatement();
+
+    return 0;
+}
