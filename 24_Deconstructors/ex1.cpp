3b6ec2cdb87ef1f4ba7c3a8b9daac4d8c457ef4c
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-05-14 01:56:24 +0530

    updated

--- 24_Deconstructors/ex1.cpp
+++ 24_Deconstructors/ex1.cpp
@@ -6,7 +6,7 @@ int main() {
     cout << "I am the constructor!" << endl;
     Wheel wObj;
     cout << "Wheely done!" << endl;
-    
+    // Deconstructor is called when the object goes out of scope, it will be called automatically.
 
     return 0;
 }
