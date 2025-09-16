10c89ad7161461628bd669ecdb0f337a385f3170
SoumyadipYT-OSS <SoumyadipYT-OSS@users.noreply.github.com>
2025-09-16 13:58:32 +0530

    Reprocedure fundamentals in Cpp

--- 07_Constructor/ex3_Header.cpp
+++
@@ -1,7 +0,0 @@
-#include "ex3_Header.h"
-#include <iostream>
-using namespace std;
-
-Example3::Example3() {
-    cout << "This is example_3 file.\n" << endl;
-}
--- 07_Constructor/ex3_main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "ex3_Header.cpp"
-using namespace std;
-
-int main() {
-    Example3 obj1;
-    return 0;
-}
