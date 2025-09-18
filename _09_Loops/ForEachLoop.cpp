3fb56b8785703b99f1c2ba5224253889d36cc6c7
SoumyadipYT-OSS <SoumyadipYT-OSS@users.noreply.github.com>
2025-09-18 18:33:03 +0530

    Add For-Each loop example in C++

---
+++ _09_Loops/ForEachLoop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int numbers[] = {10, 20, 30, 40};
+
+    for (int num : numbers)
+    {
+        cout << num << " ";
+    }
+
+    return 0;
+}
